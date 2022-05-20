3680b89985622b617fb01c42bba79e8df2d5010b
revoberliana <berlianarevo@gmail.com>
2022-05-20 14:21:44 +0700

    first commit

---
+++ database/migrations/2022_04_13_140923_create_transactions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Brick\Math\BigInteger;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Testing\Constraints\SoftDeletedInDatabase;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('users_id');
+
+            $table->text('address')->nullable();
+
+            $table->float('total_price')->default(0);
+            $table->float('shipping_price')->default(0);
+            $table->string('status')->default('PENDING');
+
+            $table->string('payment')->deafult('MANUAL');
+            
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
